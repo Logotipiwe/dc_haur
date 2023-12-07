278351735dc8c1ec51d71263f98efc51ca3217ba
logotipiwe <german.reus@bk.ru>
2023-12-07 19:47:22 +0600

    Radial gradient function through /gradient api

--- src/internal/service/images-generator.go
+++ src/internal/service/images-generator.go
@@ -4,19 +4,32 @@ import (
 	"image"
 	"image/color"
 	"image/png"
+	"log"
+	"math"
 	"net/http"
+	"strconv"
 )
 
 type ImageGenerator struct {
 }
 
 func (g *ImageGenerator) HandleGradientRequest(w http.ResponseWriter, r *http.Request) {
-	width, height := 1280, 720
-	img := image.NewRGBA(image.Rect(0, 0, width, height))
+	width, height := getFloatQueryParam(r, "w", 1280), getFloatQueryParam(r, "h", 720)
+	startX, startY := getFloatQueryParam(r, "sx", 0.0), getFloatQueryParam(r, "sy", 0.0)
+	endX, endY := getFloatQueryParam(r, "ex", 1.0), getFloatQueryParam(r, "ey", 1.0)
+
+	img := image.NewRGBA(image.Rect(0, 0, int(width), int(height)))
+
 	startColor := color.RGBA{74, 62, 255, 255}
-	endColor := color.RGBA{219, 100, 255, 255}
-	drawGradient(img, startColor, endColor)
+	endColor := color.RGBA{219, 100, 0, 255}
+
+	// Draw gradient background
+	drawGradient(img, startColor, endColor, startX, startY, endX, endY)
+
+	// Set the Content-Type header
 	w.Header().Set("Content-Type", "image/png")
+
+	// Encode the image and write it to the response writer
 	err := png.Encode(w, img)
 	if err != nil {
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
@@ -24,23 +37,49 @@ func (g *ImageGenerator) HandleGradientRequest(w http.ResponseWriter, r *http.Re
 	}
 }
 
-func drawGradient(img *image.RGBA, startColor, endColor color.RGBA) {
+func drawGradient(img *image.RGBA, startColor, endColor color.RGBA, startX, startY, endX, endY float64) {
 	bounds := img.Bounds()
+	dx := endX - startX
+	dy := endY - startY
+
 	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
-		// Calculate the interpolation factor based on the y-coordinate
-		t := float64(y-bounds.Min.Y) / float64(bounds.Max.Y-bounds.Min.Y)
-
-		// Interpolate between startColor and endColor
-		r := uint8(float64(startColor.R)*(1-t) + float64(endColor.R)*t)
-		g := uint8(float64(startColor.G)*(1-t) + float64(endColor.G)*t)
-		b := uint8(float64(startColor.B)*(1-t) + float64(endColor.B)*t)
-		a := uint8(float64(startColor.A)*(1-t) + float64(endColor.A)*t)
-
-		// Set the color for each pixel in the current row
 		for x := bounds.Min.X; x < bounds.Max.X; x++ {
+			// Calculate the interpolation factors based on the x and y coordinates
+			tX := (float64(x-bounds.Min.X) / float64(bounds.Max.X-bounds.Min.X))
+			tY := (float64(y-bounds.Min.Y) / float64(bounds.Max.Y-bounds.Min.Y))
+
+			// Calculate the position along the line defined by start and end points
+			lerpX := startX + tX*dx
+			lerpY := startY + tY*dy
+
+			// Calculate the distance between the current point and the line
+			distance := math.Sqrt(math.Pow(lerpX-float64(x), 2) + math.Pow(lerpY-float64(y), 2))
+
+			// Calculate the weight for interpolation based on the distance
+			weight := 1 - distance/math.Sqrt(math.Pow(dx, 2)+math.Pow(dy, 2))
+
+			// Interpolate between startColor and endColor
+			r := uint8(float64(startColor.R)*(1-weight) + float64(endColor.R)*weight)
+			g := uint8(float64(startColor.G)*(1-weight) + float64(endColor.G)*weight)
+			b := uint8(float64(startColor.B)*(1-weight) + float64(endColor.B)*weight)
+			a := uint8(float64(startColor.A)*(1-weight) + float64(endColor.A)*weight)
+
 			img.SetRGBA(x, y, color.RGBA{r, g, b, a})
 		}
 	}
+}
+
+func getFloatQueryParam(r *http.Request, key string, defaultValue float64) float64 {
+	value := r.URL.Query().Get(key)
+	if value == "" {
+		return defaultValue
+	}
+	floatValue, err := strconv.ParseFloat(value, 64)
+	if err != nil {
+		log.Printf("Invalid float value for %s. Using default.", key)
+		return defaultValue
+	}
+	return floatValue
 }
 
 /*
