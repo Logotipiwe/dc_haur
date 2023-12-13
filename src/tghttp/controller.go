4891f7be69cc06a60033903fc9de54232012d0bb
logotipiwe <german.reus@bk.ru>
2023-12-13 14:43:52 +0600

    Implement feedback and new question features
    The update supports the reception of user feedback and new questions that get forwarded to the owner. New services, configs, and repositories, including chat caches, have been added to ensure efficient handling of user-generated data. The bot now sends feedback and new question data to the owner's chat. Tests have been adjusted to accommodate these changes.

--- src/tghttp/controller.go
+++ src/tghttp/controller.go
@@ -3,6 +3,7 @@ package tghttp
 import (
 	"database/sql"
 	handler "dc_haur/src/internal"
+	"dc_haur/src/internal/domain"
 	"dc_haur/src/internal/repo"
 	"dc_haur/src/internal/service"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
@@ -15,18 +16,19 @@ func StartTgBot(db *sql.DB) {
 }
 
 func tgBot(db *sql.DB) {
-	bot, err := tgbotapi.NewBotAPI(config.GetConfig("BOT_TOKEN"))
+	tgbot, err := tgbotapi.NewBotAPI(config.GetConfig("BOT_TOKEN"))
+	bot := domain.Bot{Instance: tgbot}
 	if err != nil {
 		log.Panic(err)
 	}
-	bot.Debug = true
-	log.Printf("Authorized on account %s", bot.Self.UserName)
+	bot.Instance.Debug = true
+	log.Printf("Authorized on account %s", bot.Instance.Self.UserName)
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 60
-	updates := bot.GetUpdatesChan(u)
+	updates := bot.Instance.GetUpdatesChan(u)
 
 	repos := repo.NewRepositories(db)
-	services := service.NewServices(repos.Questions, repos.Decks)
+	services := service.NewServices(repos.Questions, repos.Decks, bot)
 
 	tgHandler := handler.NewHandler(services.TgMessages, services.Cache)
 
@@ -38,7 +40,7 @@ func tgBot(db *sql.DB) {
 				reply = sendUnknownCommandAnswer(update)
 			}
 			if reply != nil {
-				_, err := bot.Send(reply)
+				_, err := bot.Instance.Send(reply)
 				if err != nil {
 					println("ERROR WHILE SENDING MESSAGE!")
 					println(err)
