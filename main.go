725819e0f62a827000c6b966595b9e41521712eb
logotipiwe <german.reus@bk.ru>
2024-01-28 02:15:19 +0600

    +Swagger

--- main.go
+++ main.go
@@ -4,6 +4,18 @@ import (
 	"dc_haur/src/starter"
 )
 
+// @title           HAUR Swagger API
+// @version         1.0
+// @description     This is a HAUR server.
+// @termsOfService  http://swagger.io/terms/
+// @license.url   	http://www.apache.org/licenses/LICENSE-2.0.html
+// @host      		logotipiwe.ru
+//TODO dynamically specify host
+
+// @BasePath  /api/v1
+// @securityDefinitions.basic  BasicAuth
+// @externalDocs.description  OpenAPI
+// @externalDocs.url          https://swagger.io/resources/open-api/
 func main() {
 	starter.StartApp()
 }
--- src/http/controller.go
+++ src/http/controller.go
@@ -7,16 +7,24 @@ import (
 	"github.com/Logotipiwe/dc_go_auth_lib/auth"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	config "github.com/logotipiwe/dc_go_config_lib"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 	"image/png"
 	"log"
 	"net/http"
 
+	_ "dc_haur/docs"
 	"github.com/gin-gonic/gin"
 )
 
 const IntegrationTestPrefix = "/api/v1/integration-test"
 
+type Controller struct {
+	services *service.Services
+}
+
 func StartServer(services *service.Services) {
+	controller := Controller{services: services}
 	router := gin.Default()
 	integrationTestingRoutes := router.Group(IntegrationTestPrefix)
 
@@ -68,51 +76,84 @@ func StartServer(services *service.Services) {
 
 	apiV1 := router.Group("/api/v1")
 
-	apiV1.GET("/decks", doWithErr(func(c *gin.Context) error {
-		decks, err := services.Decks.GetDecks()
-		if err != nil {
-			return err
-		}
-		c.JSON(http.StatusOK, decks)
-		return nil
-	}))
+	apiV1.GET("/decks", doWithErr(controller.GetDecks()))
 
-	apiV1.GET("/levels", doWithErr(func(c *gin.Context) error {
-		deckId := c.Query("deckId")
-		levels, err := services.Repos.Levels.GetLevelsByDeckId(deckId)
-		if errors.Is(err, repo.NoLevelsErr) {
-			c.JSON(http.StatusBadRequest, gin.H{"error": "No levels found by deck id " + deckId})
-			return nil
-		} else if err != nil {
-			return err
-		}
-		c.JSON(http.StatusOK, levels)
-		return nil
-	}))
+	apiV1.GET("/levels", doWithErr(controller.GetLevels()))
 
-	apiV1.GET("/question", doWithErr(func(c *gin.Context) error {
-		levelID := c.Query("levelId")
-		question, err := services.Questions.GetRandQuestion(levelID)
-		if err != nil {
-			return err
-		}
+	apiV1.GET("/question", doWithErr(controller.GetQuestion()))
 
-		if clientId, exists := c.GetQuery("clientId"); exists {
-			err := services.Repos.History.Insert(clientId, question)
-			if err != nil {
-				return err
-			}
-		}
-
-		c.JSON(http.StatusOK, question)
-		return nil
-	}))
+	apiV1.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
 	port := config.GetConfigOr("CONTAINER_PORT", "80")
 	log.Println("Starting server on port " + port)
 	err := router.Run(":" + port)
 	if err != nil {
 		panic(err)
+	}
+}
+
+// GetDecks godoc
+// @Summary      Get all available decks
+// @Produce      json
+// @Success      200  {array} domain.Deck
+// @Router       /decks [get]
+func (c Controller) GetDecks() func(c *gin.Context) error {
+	return func(ctx *gin.Context) error {
+
+		decks, err := c.services.Decks.GetDecks()
+		if err != nil {
+			return err
+		}
+		ctx.JSON(http.StatusOK, decks)
+		return nil
+	}
+}
+
+// GetLevels godoc
+// @Summary      Get levels from specified deck
+// @Param 		 deckId query string true "Id of deck for which selecting levels"
+// @Produce      json
+// @Success      200  {array} domain.Level
+// @Router       /levels [get]
+func (c Controller) GetLevels() func(ctx *gin.Context) error {
+	return func(ctx *gin.Context) error {
+		deckId := ctx.Query("deckId")
+		levels, err := c.services.Repos.Levels.GetLevelsByDeckId(deckId)
+		if errors.Is(err, repo.NoLevelsErr) {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": "No levels found by deck id " + deckId})
+			return nil
+		} else if err != nil {
+			return err
+		}
+		ctx.JSON(http.StatusOK, levels)
+		return nil
+	}
+}
+
+// GetQuestion godoc
+// @Summary      Get random question by selected level
+// @Param 		 levelId query string true "Id of level for which selecting question"
+// @Param 		 clientId query string true "Client id - differs clients from each other. Needed for ordering random questions for each client/"
+// @Produce      json
+// @Success      200  {object} domain.Question
+// @Router       /question [get]
+func (c Controller) GetQuestion() func(ctx *gin.Context) error {
+	return func(ctx *gin.Context) error {
+		levelID := ctx.Query("levelId")
+		question, err := c.services.Questions.GetRandQuestion(levelID)
+		if err != nil {
+			return err
+		}
+
+		if clientId, exists := ctx.GetQuery("clientId"); exists {
+			err := c.services.Repos.History.Insert(clientId, question)
+			if err != nil {
+				return err
+			}
+		}
+
+		ctx.JSON(http.StatusOK, question)
+		return nil
 	}
 }
 
